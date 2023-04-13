3cf3004a70a6c0519ea967a6d429a0d3bc473e18
presh <ogwaraprecious8@gmail.com>
2023-04-13 11:56:40 -0700

    i made changes to the website

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let country = "Nigeria"
+  return (
+    <>
+    <header>
+     <h1>Welcome to my website!</h1>
+     <nav>
+       <ul>
+         <li><a href="#">Home</a></li>
+         <li><a href="#">About</a></li>
+         <li><a href="#">Services</a></li>
+         <li><a href="#">Contact</a></li>
+       </ul>
+     </nav>
+   </header>
+    <div>
+      The name of my country is {country}
+    </div>
+   <main>
+     <section>
+       <h2>About Us</h2>
+       <p>
+         We are a small team of web developers based in the United States. We
+         specialise in creating modern and responsive websites for businesses
+         and individuals.
+         </p>
+     </section>
+     <section>
+       <h2>Our Services</h2>
+       <ul>
+         <li>Web Design</li>
+         <li>Web Development</li>
+         <li>E-commerce Solutions</li>
+         <li>Search Engine Optimization (SEO)</li>
+       </ul>
+     </section>
+   </main>
+
+   </>
+  );
+}
+
+export default App;
